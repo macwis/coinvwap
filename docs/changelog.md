ad5b3fd8a5abf5d19935f96fdfbf6ad3ed89296b
Maciej <mw@iceam.com>
2022-02-16 18:28:12 +0100

    typo fix; changelog update;

--- docs/changelog.md
+++ docs/changelog.md
@@ -4,6 +4,7 @@ Changelog
 
 (unreleased)
 ------------
+- More tests; more README; more checks; more coverage. [Maciej]
 - Refactor; pylint fixes; package build prep; pytest config. [Maciej]
 - Poc. [Maciej]
 
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
         url="wss://ws-feed.exchange.coinbase.com/",
         product_ids=["BTC-USD", "ETH-USD", "ETH-BTC"],
         channel="ticker",
-        type_field="ticker"
+        type_field="ticker",
         price_field="price",
         quantity_field="last_size",
     )
